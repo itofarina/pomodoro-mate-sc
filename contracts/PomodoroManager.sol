3a9a5368ee5773df66dca4bd528ec27ab2839f98
Alvaro Fariña <farinalvaro@gmail.com>
2021-06-15 19:07:12 -0300

    feat: redeem token + adds test

--- contracts/PomodoroManager.sol
+++ contracts/PomodoroManager.sol
@@ -5,7 +5,6 @@ import "./PMToken.sol";
 
 contract PomodoroManager {
     string public name = "Pomodoro Manager";
-    address public owner;
     PMToken public pmToken;
 
     address[] public doers; // people who completes pomodoros
@@ -13,12 +12,11 @@ contract PomodoroManager {
 
     constructor(PMToken _pmToken) {
         pmToken = _pmToken;
-        owner = msg.sender;
     }
 
     function redeemToken(address _doer) public {
         // TODO: should require to not be called by the same doer in less than 30 minutes
-        pmToken.transferFrom(owner, _doer, 1 * 10**18);
+        pmToken.transfer(_doer, 1);
         doers.push(_doer);
         pomodorosCompleted[_doer] = pomodorosCompleted[_doer] + 1;
     }
--- test/PomodoroManager.test.js
+++ test/PomodoroManager.test.js
@@ -3,26 +3,77 @@ const {
   expectEvent,
   expectRevert,
   balance,
-  constants
+  constants,
+  ether
 } = require('@openzeppelin/test-helpers')
 const upgrades = require('@openzeppelin/truffle-upgrades')
-const { toBN } = require('web3-utils')
+const { toBN, toWei } = require('web3-utils')
 
 const { expect } = require('chai')
 
 const PomodoroManager = artifacts.require('PomodoroManager')
 const PMToken = artifacts.require('PMToken')
 
-contract('PomodoroManager', ([owner, randomDoer]) => {
+contract('PomodoroManager', ([owner, randomDoer1, randomDoer2]) => {
   let pmToken, pomodoroManager
+  const initialSupply = 100000000
+  const managerInitialBalance = 100
 
   before(async () => {
     // Load Contracts
-    pmToken = await PMToken.new()
-    pomodoroManager = await PomodoroManager.new(pmToken.address)
+    pmToken = await PMToken.new(owner, initialSupply)
+    pomodoroManager = await PomodoroManager.new(pmToken.address, { from: owner })
+
+    // Send tokens to pomodoro manager contract
+    await pmToken.transfer(pomodoroManager.address, managerInitialBalance, { from: owner })
   })
 
-  describe('Issuing tokens', async () => {
+  // TODO: move to PMToken.test.js
+  describe('PMToken deployment', () => {
+    it('has a name', async () => {
+      const name = await pmToken.name()
+      expect(name).to.eql('Pomodoro Mate Token')
+    })
 
+    it('owner has proper balance', async () => {
+      const ownerBalance = await pmToken.balanceOf(owner)
+      expect(ownerBalance.toString()).to.eql(
+        (initialSupply - managerInitialBalance).toString()
+      )
+    })
   })
+
+  describe('Pomodoro Manager deployment', () => {
+    it('has a name', async () => {
+      const name = await pomodoroManager.name()
+      expect(name).to.eql('Pomodoro Manager')
+    })
+
+    it('has initial balance', async () => {
+      const balance = await pmToken.balanceOf(pomodoroManager.address)
+      expect(balance.toString()).to.eql(managerInitialBalance.toString())
+    })
+  })
+
+  describe('Redeem tokens', async () => {
+    before(async () => {
+      await pomodoroManager.redeemToken(randomDoer1)
+    })
+
+    it('should give a token to random doer', async () => {
+      const randomDoerBalance = await pmToken.balanceOf(randomDoer1)
+      expect(randomDoerBalance.toString()).to.eql('1')
+    })
+
+    it('contract should have 1 less token', async () => { })
+
+    it('should add account to doers', async () => { })
+
+    it('doer should have 1 pomodoro completed', async () => { })
+
+    it('should reject giving 1 more before 30 minutes to the same account', () => { })
+
+    it('should give 1 token to another random doer', () => { })
+  })
+
 })
